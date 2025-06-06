2d10f95ba2844be3de04f46b80245f8432f20b96
kidlatpogi <dzeref4000@gmail.com>
2025-06-06 14:51:41 +0800

    Update room_reservation.sql
    update QUERY of Database

--- room_reservation.sql
+++ room_reservation.sql
@@ -1,39 +1,61 @@
 CREATE DATABASE room_reservation;
 USE room_reservation;
 
+-- for LOGIN and SIGNUP of the users
 CREATE TABLE USERS(
-	user_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT
-    , email VARCHAR(50) NOT NULL
-    , password VARCHAR(50) NOT NULL
+    user_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL
 );
 
+-- usders details connected to USERS table
 CREATE TABLE USER_DETAILS(
-	userDetails_id INT PRIMARY KEY NOT NULL
-    , fname VARCHAR(30) NOT NULL DEFAULT ' '
-    , lname VARCHAR(30) NOT NULL DEFAULT ' '
-    , user_id INT UNIQUE
-    , CONSTRAINT user_id_fk FOREIGN KEY(user_id) REFERENCES USERS(user_id)
+    userDetails_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    fname VARCHAR(30) NOT NULL DEFAULT ' ',
+    lname VARCHAR(30) NOT NULL DEFAULT ' ',
+    image BLOB NULL,
+    user_id INT UNIQUE,
+    CONSTRAINT user_id_fk FOREIGN KEY(user_id) REFERENCES USERS(user_id)
 );
 
+-- for rooms floor and type
 CREATE TABLE ROOMS(
-	room_id INT PRIMARY KEY NOT NULL
-    , floor ENUM('4th','5th')
-    , room_type ENUM('Lecture', 'Laboratory')
+    room_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    floor ENUM('4th','5th') NOT NULL,
+    room_type ENUM('Lecture', 'Laboratory') NOT NULL
 );
 
+-- rooms descriptions
+CREATE TABLE ROOM_DETAILS (
+    room_detail_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    room_id INT NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    CONSTRAINT fk_room_details FOREIGN KEY(room_id) REFERENCES ROOMS(room_id)
+);
+
+-- reservation status
+CREATE TABLE RESERVATION_STATUS(
+    status_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    status ENUM('Ongoing', 'Completed', 'Canceled') NOT NULL
+);
+
+-- for users reservations, connected to USERS_DETAILES, ROOMS, RESERVATION_STATUS
 CREATE TABLE RESERVATION(
-	reservation_id INT PRIMARY KEY NOT NULL
-    , reservation_date DATE NOT NULL
-    , start_time TIME NOT NULL
-    , end_time TIME NOT NULL
-    , userDetails_id INT
-    , room_id INT
-    , CONSTRAINT userDetails_id_FK FOREIGN KEY(userDetails_id) REFERENCES USER_DETAILS(userDetails_id)
-    , CONSTRAINT room_id_fk FOREIGN KEY(room_id) REFERENCES ROOMS(room_id)
+    reservation_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    reservation_date DATE NOT NULL,
+    start_time TIME NOT NULL,
+    end_time TIME NOT NULL,
+    userDetails_id INT,
+    room_id INT,
+    status_id INT,
+    CONSTRAINT fk_userDetails FOREIGN KEY(userDetails_id) REFERENCES USER_DETAILS(userDetails_id),
+    CONSTRAINT fk_room FOREIGN KEY(room_id) REFERENCES ROOMS(room_id),
+    CONSTRAINT fk_status FOREIGN KEY(status_id) REFERENCES RESERVATION_STATUS(status_id)
 );
 
+-- connected to ROOMS table
 CREATE TABLE ROOM_SCHEDULE (
-    schedule_id INT PRIMARY KEY AUTO_INCREMENT,
+    schedule_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     room_id INT NOT NULL,
     day_of_week ENUM('Monday','Tuesday','Wednesday','Thursday','Friday','Saturday') NOT NULL,
     start_time TIME NOT NULL,
@@ -44,24 +66,3 @@ CREATE TABLE ROOM_SCHEDULE (
     is_occupied BOOLEAN NOT NULL DEFAULT TRUE,
     CONSTRAINT fk_room_schedule FOREIGN KEY(room_id) REFERENCES ROOMS(room_id)
 );
-
-CREATE VIEW reservation_view AS
-SELECT 
-    u.user_id,
-    ud.lname,
-    ud.fname,
-    r.room_id,
-    r.reservation_date AS date,
-    r.start_time,
-    r.end_time
-FROM RESERVATION r
-JOIN USER_DETAILS ud ON r.userDetails_id = ud.userDetails_id
-JOIN USERS u ON ud.user_id = u.user_id;
-
-SELECT * FROM reservation_view;
-
-INSERT INTO ROOM_SCHEDULE(room_id, day_of_week, start_time, end_time, subject_code, section, instructor, is_occupied)
-	VALUES (401, 'Monday', '07:00:00', '09:40:00', 'ABC231', 'ABCOM16X', 'R. BUELO', TRUE);
-
-
-
